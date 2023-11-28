0e7a9da06418252e94cf709ba053322c3f0b4fa8
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-11-28 14:01:46 +0200

    1.1 done

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -2,11 +2,11 @@
 
 
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age: number = 50;
+let name: string = 'Max';
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: void;
+let callback = (a: number) => { return 100 + a };
 
 export {};
